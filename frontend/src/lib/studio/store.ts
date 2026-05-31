9c2219ddc9dfa2b3b13261eb3534952e1301990c
cptrodgers <cptrodgers@gmail.com>
2026-05-31 18:09:00 +0700

    chore: remove store file

--- frontend/src/lib/studio/store.ts
+++
@@ -1,5 +0,0 @@
-// Barrel re-export. Prefer importing directly from the stores/ subdirectory.
-export { useProfileStore } from "./stores/profile-store";
-export { useWidgetStore } from "./stores/widget-store";
-export { useTestStore } from "./stores/test-store";
-export * from "./stores/types";
---
+++ frontend/src/lib/studio/stores/index.ts
@@ -0,0 +1,5 @@
+// Barrel re-export. Prefer importing directly from the stores/ subdirectory.
+export { useProfileStore } from "./profile-store";
+export { useWidgetStore } from "./widget-store";
+export { useTestStore } from "./test-store";
+export * from "./types";
